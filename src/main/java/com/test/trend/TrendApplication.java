1f22f73434594631faac25c2766be6935e1ca453
Sunmin <sunmin323233@gmail.com>
2025-12-01 16:51:50 +0900

    init

---
+++ src/main/java/com/test/trend/TrendApplication.java
@@ -0,0 +1,13 @@
+package com.test.trend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendApplication.class, args);
+    }
+
+}
