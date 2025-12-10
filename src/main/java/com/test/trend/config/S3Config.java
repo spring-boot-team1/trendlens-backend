efddd6edc7e504462cf9b751e4394de20d8fdfe7
Sunmin <sunmin323233@gmail.com>
2025-12-10 12:30:04 +0900

    fix: s3 인증 관련 코드 추가

--- src/main/java/com/test/trend/config/S3Config.java
+++ src/main/java/com/test/trend/config/S3Config.java
@@ -1,9 +1,12 @@
 package com.test.trend.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -12,13 +15,23 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @RequiredArgsConstructor
 @EnableConfigurationProperties(S3Properties.class)
 public class S3Config {
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+
     /**
      * S3에 접근하는 객체 생성
      * @return S3 접근 객체
      */
     @Bean
     public S3Client s3Client() {
-        return S3Client.builder().region(Region.AP_NORTHEAST_2).build();
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
     }
 
     /**
@@ -27,6 +40,9 @@ public class S3Config {
      */
     @Bean
     public S3Presigner s3Presigner(){
-        return S3Presigner.builder().region(Region.AP_NORTHEAST_2).build();
+        return S3Presigner.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
     }
 }
