c9204b4f7cea0d414241a83f46e878391819a029
ppjh94 <wleer157@gmail.com>
2025-12-17 10:38:57 +0900

    refacor: mapper 컬럼값 수정

--- src/main/java/com/test/trend/domain/payment/subscription/mapper/UserSubscriptionMapper.java
+++ src/main/java/com/test/trend/domain/payment/subscription/mapper/UserSubscriptionMapper.java
@@ -9,12 +9,12 @@ import com.test.trend.domain.payment.subscription.entity.UserSubscription;
 @Mapper(componentModel = "spring")
 public interface UserSubscriptionMapper {
 
-	@Mapping(source = "subscriptionPlan.seqSubscriptionPlan", target = "seqSubscriptionPlan")
-    @Mapping(source = "subscriptionPlan.planName", target = "planName")
+	@Mapping(source = "seqSubscriptionPlan.seqSubscriptionPlan", target = "seqSubscriptionPlan")
+    @Mapping(source = "seqSubscriptionPlan.planName", target = "planName")
     UserSubscriptionDTO toDto(UserSubscription entity);
 
     /** Entity 생성 시 DTO → Entity 직접 매핑할 일 거의 없음 → ignore 처리 */
-    @Mapping(target = "subscriptionPlan", ignore = true)
+    @Mapping(target = "seqSubscriptionPlan", ignore = true)
     @Mapping(target = "seqUserSub", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     UserSubscription toEntity(UserSubscriptionDTO dto);
